9194a754a23cdea084f3d997571a3965b7c0c962
Evan <innevatorg@gmail.com>
2023-12-08 23:58:27 +0800

    add Movie and MovieService test

--- MovieFeature/MovieFeatureTests/MovieFeatureTests.swift
+++ MovieFeature/MovieFeatureTests/MovieFeatureTests.swift
@@ -6,31 +6,44 @@
 //
 
 import XCTest
-@testable import MovieFeature
+import MovieFeature
+
+struct Movie: Equatable {
+    let name: String
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
+protocol MovieService {
+    func loadMovies(completion: (Result<[Movie], Error>) -> Void)
+}
+
+struct MovieAPIService: MovieService {
+    let movies: [Movie]
+    
+    func loadMovies(completion: (Result<[Movie], Error>) -> Void) {
+        completion(.success(movies))
+    }
+}
 
 final class MovieFeatureTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_loadMovies() {
+        var getMovies: [Movie] = []
+        let testMovies = [Movie(name: "Movie1"), Movie(name: "Movie2")]
+        let service = MovieAPIService(movies: testMovies)
+        
+        service.loadMovies { result in
+            switch result {
+            case .success(let movies):
+                getMovies = movies
+            case .failure(_):
+                break
+            }
         }
+        
+        XCTAssertEqual(getMovies, testMovies)
     }
-
 }
